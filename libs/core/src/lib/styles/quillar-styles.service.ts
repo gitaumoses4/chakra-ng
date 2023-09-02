4d9ed981966b8ab4d08be181f36f2000a9229e39
Moses Gitau <gitaumoses4@gmail.com>
2023-09-02 17:06:42 +0300

    Update global styles

--- libs/core/src/lib/styles/quillar-styles.service.ts
+++ libs/core/src/lib/styles/quillar-styles.service.ts
@@ -3,7 +3,7 @@ import { Interpolation, SerializedStyles, serializeStyles } from "@emotion/seria
 import { DOCUMENT } from "@angular/common";
 import { QuillarCacheService } from "./quillar-cache.service";
 import { ThemeService } from "../config";
-import { insertStyles, registerStyles } from "@emotion/utils";
+import { registerStyles } from "@emotion/utils";
 
 type Styles = Array<Interpolation<any> | TemplateStringsArray>;
 
@@ -21,39 +21,27 @@ export class QuillarStylesService {
   }
 
   public attachGlobalStyles(styles: Styles) {
-    const cache = this.cacheService.cache();
-    const serialized = serializeStyles(styles, cache.registered, this.themeService.getTheme());
-
-    const key = `${cache.key}-global`;
-
-    const sheet = new (cache.sheet.constructor as any)({
-      key,
-      nonce: cache.sheet.nonce,
-      container: cache.sheet.container,
-      speedy: (cache.sheet as any).isSpeedy,
-    });
-
-    if (serialized.next !== undefined) {
-      insertStyles(cache, serialized.next, true);
-    }
-
-    if (sheet.tags.length) {
-      const element = sheet.tags[sheet.tags.length - 1].nextElementSibling;
-      sheet.before = element as any;
-      sheet.flush();
-    }
-
-    cache.insert(``, serialized, sheet, false);
+    this.attachStyles(styles);
   }
 
-  public attachStyles(styles: Array<Interpolation<any> | TemplateStringsArray>, element: HTMLElement) {
+  public attachStyles(styles: Array<Interpolation<any> | TemplateStringsArray>, element?: HTMLElement) {
     const cache = this.cacheService.cache();
     const serialized = serializeStyles(styles, cache.registered, this.themeService.getTheme());
 
     let current: SerializedStyles | undefined = serialized;
 
     while (current !== undefined) {
-      const className = Array.from(element.classList).find((className) => cache.registered[`${cache.key}-${className}`]) || current.name;
+      let className = current.name;
+
+      if (element) {
+        const elementClassName = Array.from(element.classList).find((className) => cache.registered[`${cache.key}-${className}`]);
+        if (elementClassName) {
+          className = elementClassName;
+        }
+      } else {
+        className = "global";
+      }
+
       const key = `${cache.key}-${className}`;
 
       const node = this.document.querySelector(`style[data-emotion="${key}"]`);
@@ -66,11 +54,14 @@ export class QuillarStylesService {
       });
 
       if (node) {
-        node.innerHTML = `.${className} { ${serialized.styles} }`;
+        node.innerHTML = element ? `.${className} { ${serialized.styles} }` : serialized.styles;
       } else {
         registerStyles(cache, current, false);
-        cache.insert(`.${serialized.name}`, serialized, sheet, false);
-        this.renderer.addClass(element, className);
+        cache.insert(element ? `.${serialized.name}` : "", serialized, sheet, false);
+
+        if (element) {
+          this.renderer.addClass(element, className);
+        }
       }
 
       current = current.next;
